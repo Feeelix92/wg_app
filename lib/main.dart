dabf06d754b832dfd31d6bc7942c35d26fb50f5e
Jerome Helzel <jerome.helzel@ilume.de>
2023-08-04 13:15:39 +0200

    Added and configured Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:wg_app/data/constants.dart';
 import 'package:wg_app/routes/app_router.dart';
+
+import 'firebase_options.dart';
 
 Future main() async {
   // To load the .env file contents into dotenv.
@@ -9,6 +12,13 @@ Future main() async {
   // Ensure that the filename corresponds to the path in step 1 and 2.
   await dotenv.load(fileName: ".env");
   runApp(MyApp());
+
+  // Firebase auf für entsprechende Platform konfigurieren
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
 }
 
 class MyApp extends StatelessWidget {
