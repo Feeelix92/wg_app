a01a12115e19b786e75afc76414f9c64a85d2e2c
Felix Maurer <Feeelix92@users.noreply.github.com>
2023-09-18 09:52:02 +0200

    added tooltips for membercircles

--- lib/widgets/build_member_circle.dart
+++ lib/widgets/build_member_circle.dart
@@ -4,20 +4,39 @@ import 'package:wg_app/data/constants.dart';
 Widget buildMemberCircle(String name, double size, double brightness) {
   Color circleColor = increaseBrightness(convertToColor(name), brightness);
   double fontSize = size / 2;
-  return Container(
-    width: size,
-    height: size,
-    decoration: BoxDecoration(
-      shape: BoxShape.circle,
-      color: circleColor,
-    ),
-    child: Center(
-      child: Text(
-        name[0],
-        style: TextStyle(
-          fontSize: fontSize,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
+  return GestureDetector(
+    onTap: () {},
+    child: Tooltip(
+      message: name,
+      showDuration: const Duration(seconds: 3),
+      textStyle: const TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.bold,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(
+          color: circleColor,
+          width: 1,
+        ),
+        borderRadius: const BorderRadius.all(Radius.circular(20)),
+      ),
+      child: Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: circleColor,
+        ),
+        child: Center(
+          child: Text(
+            name[0],
+            style: TextStyle(
+              fontSize: fontSize,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
         ),
       ),
     ),
