7fcaa9546b264df18bbaa11272d7b31da526d85d
Felix Maurer <Feeelix92@users.noreply.github.com>
2023-09-22 13:11:21 +0200

    wip

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,41 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:wg_app/main.dart';
+import 'package:wg_app/providers/household_provider.dart';
+import 'package:wg_app/screens/home_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  testWidgets('HomeScreen Widget Test', (WidgetTester tester) async {
+    // Initialisieren Sie Firebase für die Tests
+    await Firebase.initializeApp();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Erstellen Sie den Mock für Firestore und Authentifizierung
+    final fakeFirestore = FakeFirebaseFirestore();
+    final auth = MockFirebaseAuth(signedIn: false);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Erstellen Sie Ihre Widget-Tests
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          // Verwenden Sie die Mocks, um Ihre Provider zu initialisieren
+          Provider<FirebaseFirestore>(create: (_) => fakeFirestore),
+          Provider<FirebaseAuth>(create: (_) => auth),
+          ChangeNotifierProvider<HouseholdProvider>(
+            create: (_) => HouseholdProvider(),
+          ),
+        ],
+        child: const MaterialApp(
+          home: HomeScreen(),
+        ),
+      ),
+    );
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Führen Sie Ihre Tests durch, indem Sie Interaktionen simulieren und erwartete Ergebnisse überprüfen.
+    // Zum Beispiel, testen Sie das Laden von Daten, das Navigieren zu anderen Bildschirmen, usw.
   });
 }
