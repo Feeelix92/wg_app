4c78703aad5c20448e055dc85d071303524e786d
Felix Maurer <Feeelix92@users.noreply.github.com>
2023-09-26 08:20:32 +0200

    added indicator for pie chart

--- lib/screens/finance_screen.dart
+++ lib/screens/finance_screen.dart
@@ -7,6 +7,7 @@ import '../providers/household_provider.dart';
 import '../widgets/color_functions.dart';
 import '../widgets/build_member_circle.dart';
 import '../widgets/navigation/app_drawer.dart';
+import '../widgets/pie_chart/indicator.dart';
 import '../widgets/text/h1.dart';
 
 /// Der Finanzscreen zeigt Ausgaben der Mitglieder eines Haushalts an.
@@ -24,73 +25,119 @@ class FinanceScreen extends StatefulWidget {
 }
 
 class _FinanceScreenState extends State<FinanceScreen> {
+  Future<Map<String, dynamic>?> fetchMemberExpenses() async {
+    final householdProvider =
+    Provider.of<HouseholdProvider>(context, listen: false);
+
+    final memberExpenses =
+    await householdProvider.calculateMemberExpenses(widget.householdId);
+
+    if (memberExpenses.isEmpty ||
+        memberExpenses.values.every((value) => value['expense'] == 0)) {
+      // Handle the case where there is no data or all expenses are zero.
+      return null;
+    }
+
+    return memberExpenses;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<HouseholdProvider>(
-        builder: (context, householdProvider, child) {
-      return Scaffold(
-        appBar: const CustomAppBar(),
-        endDrawer: const AppDrawer(),
-        body: Center(
-          child: Column(
-            children: [
-              const H1(text: 'Ausgaben'),
-              Expanded(
-                child: AspectRatio(
-                  aspectRatio: 20,
+      builder: (context, householdProvider, child) {
+        return Scaffold(
+          appBar: const CustomAppBar(),
+          endDrawer: const AppDrawer(),
+          body: Center(
+            child: Column(
+              children: [
+                const H1(text: 'Ausgaben'),
+                Expanded(
                   child: AspectRatio(
-                    aspectRatio: 10,
-                    child: FutureBuilder(
-                      future: householdProvider
-                          .calculateMemberExpenses(widget.householdId),
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return const Center(
-                              child: CircularProgressIndicator());
-                        } else if (snapshot.hasError) {
-                          return Center(
-                              child: Text('Error: ${snapshot.error}'));
-                        } else if (!snapshot.hasData ||
-                            (snapshot.data as Map<String, dynamic>).isEmpty) {
-                          return const Center(
-                              child: Text('Keine Daten verfügbar'));
-                        } else if (!snapshot.hasData ||
-                            (snapshot.data as Map<String, dynamic>)
-                                .values
-                                .every((value) => value['expense'] == 0)) {
-                          return const Center(
-                              child: Text(
-                                  'Es sind noch keine Ausgaben vorhanden'));
-                        } else {
-                          final memberExpenses =
-                              snapshot.data as Map<String, dynamic>;
-                          return PieChart(
-                            PieChartData(
-                              borderData: FlBorderData(
-                                show: false,
+                    aspectRatio: 20,
+                    child: AspectRatio(
+                      aspectRatio: 10,
+                      child: FutureBuilder(
+                        future: fetchMemberExpenses(),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const Center(
+                                child: CircularProgressIndicator());
+                          } else if (snapshot.hasError) {
+                            return Center(
+                                child: Text('Error: ${snapshot.error}'));
+                          } else if (snapshot.data == null) {
+                            /// Keine Daten verfügbar.
+                            return const Center(child: Text('Keine Daten verfügbar'));
+                          } else {
+                            final memberExpenses =
+                            snapshot.data as Map<String, dynamic>;
+                            return PieChart(
+                              PieChartData(
+                                borderData: FlBorderData(
+                                  show: false,
+                                ),
+                                sectionsSpace: 0,
+                                centerSpaceRadius: 10,
+                                centerSpaceColor: Colors.white,
+                                sections: showingSections(memberExpenses),
                               ),
-                              sectionsSpace: 0,
-                              centerSpaceRadius: 10,
-                              centerSpaceColor: Colors.white,
-                              sections: showingSections(memberExpenses),
-                            ),
-                          );
-                        }
-                      },
+                            );
+                          }
+                        },
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+                FutureBuilder(
+                  future: fetchMemberExpenses(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return const Center(
+                          child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return Center(
+                          child: Text('Error: ${snapshot.error}'));
+                    } else if (snapshot.data == null) {
+                      /// Keine Daten verfügbar.
+                      return const Center(child: Text('Keine Daten verfügbar'));
+                    } else {
+                      final memberExpenses =
+                      snapshot.data as Map<String, dynamic>;
+                      return SizedBox(
+                        height: 200,
+                        child: SingleChildScrollView(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Wrap(
+                              alignment: WrapAlignment.center,
+                              spacing: 5.0,
+                              children: [
+                                ...memberExpenses.entries.map((memberId) {
+                                  final memberData = memberId.value;
+                                  final memberExpense = memberData['expense'];
+                                  final username = memberData['username'];
+                                  return buildIndicator(username, memberExpense);
+                                }).toList(),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                ),
+              ],
+            ),
           ),
-        ),
-      );
-    });
+        );
+      },
+    );
   }
 
-  List<PieChartSectionData> showingSections(
-      Map<String, dynamic> memberExpenses) {
+  List<PieChartSectionData> showingSections(Map<String, dynamic> memberExpenses) {
     final List<PieChartSectionData> sections = [];
 
     memberExpenses.forEach((memberId, memberData) {
@@ -102,6 +149,14 @@ class _FinanceScreenState extends State<FinanceScreen> {
     });
 
     return sections;
+  }
+
+  Indicator buildIndicator(String username, double memberExpense) {
+    return Indicator(
+      color: increaseBrightness(convertToColor(username), 0.2),
+      text: '$username: ${memberExpense.toStringAsFixed(2)} €',
+      isSquare: false,
+    );
   }
 
   PieChartSectionData buildPieChartSectionData(String personName, double personValue, double percentageOfTotal) {
